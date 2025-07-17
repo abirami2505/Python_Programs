4d7313e6fb77013d8f6bf20abbc28b7144b5c583
abirami2505 <abiramik2503@gmail.com>
2025-07-17 22:35:29 +0530

    Add files via upload

---
+++ forloops.py
@@ -0,0 +1,36 @@
+names=["abi","sathya","prathi","nava","manu"]
+for i in names:
+    print(i)
+
+for i in "prathi":
+    print(i)
+
+for i in names:
+    print(i)
+    if i=="nava":
+        break
+
+for i in names:
+    if i =="nava":
+        break
+    print(i)
+
+for i in names:
+    if i=="nava":
+        continue
+    print(i)
+
+for i in names:
+    print(i)
+    if i=="nava":
+        continue
+
+for i in range(6):
+    print(i)
+
+for i in range(2,6):
+    print(i)
+
+for i in range (2,20,2):
+    print(i)
+
